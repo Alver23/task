608a8389c8aef35e6d07bb3f4e1b7e8aa9b00670
Alver Grisales Ortega <viga.23@hotmail.com>
2017-01-14 18:37:02 -0500

    Se crea migracion de usuarios

---
+++ database/migrations/2017_01_14_180636_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('uuid');
+            $table->string('first_name', 80);
+            $table->string('last_name', 80);
+            $table->string('email');
+            $table->string('token');
+            $table->string('ip_address', 15)->default('127.0.0.1');
+            $table->integer('owner_user_id')->unsigned()->nullable();
+            $table->integer('updater_user_id')->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('owner_user_id')->references('id')->on('users');
+            $table->foreign('updater_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
