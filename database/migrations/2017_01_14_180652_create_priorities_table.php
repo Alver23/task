c48174c54c82d95911112d51b7aeae1e8cb40c73
Alver Grisales Ortega <viga.23@hotmail.com>
2017-01-14 18:37:30 -0500

    Se crea migracion de prioridades

---
+++ database/migrations/2017_01_14_180652_create_priorities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrioritiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('priorities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 80);
+            $table->string('description', 8192)->nullable();
+            $table->string('ip_address', 15)->default('127.0.0.1');
+            $table->integer('owner_user_id')->unsigned()->nullable();
+            $table->integer('updater_user_id')->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('owner_user_id')->references('id')->on('users');
+            $table->foreign('updater_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('priorities');
+    }
+}
